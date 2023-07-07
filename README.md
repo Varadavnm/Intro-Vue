834cb654c94ddb1cee3c10798994748c0be83f3a
satrtingpyth <varadavnm@gmail.com>
2023-07-07 08:38:29 +0530

    Vue.js Introductory files

---
+++ README.md
@@ -0,0 +1,1 @@
+This is an introductory file specifically to learn Vue.js
---
+++ app.js
@@ -0,0 +1,6 @@
+new VTTCue({
+    // options object:
+    // all properties are options
+    el: "#app",
+    template: "<h1> Hello User </h1>"
+})
