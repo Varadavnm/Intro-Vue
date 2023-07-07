bbcf80c0cf54d0f0b0edce5a052124b9dae0023f
satrtingpyth <varadavnm@gmail.com>
2023-07-07 11:32:59 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-new VTTCue({
-    // options object:
-    // all properties are options
-    el: "#app",
-    template: "<h1> Hello User </h1>"
-})
+new Vue({
+    el: '#app',
+    template:`
+    <div>
+    <h1> {{blog.author}} </h1>
+    <h1> {{blog.Title}}</h1>
+    <h1 v-if="on" v-bind:class="className"> {{books.Title}} </h1>
+    <h1 v-else> No books found </h1>
+    <h1 v-if="on"> {{books.date}}</h1>
+    </div>`,
+    data: {
+        blog: {"author": "Varada", "Title" : "Live session"},
+        books: {"Title": "New Vueapp", "date": "12/04/23"},
+        on: true,
+        className: "Danger",
+    }
+  })
+
+
+
+
+
+
